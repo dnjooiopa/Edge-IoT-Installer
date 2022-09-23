dc2a9d77beb70c1c1c8e864d14d2f8d3165df2b5
Satori Nakamoto <dnjooiopa@gmail.com>
2022-09-23 07:06:58 +0700

    Update install-docker.sh

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -3,8 +3,8 @@
 curl -sSL https://get.docker.com | sh
 sudo usermod -aG docker ${USER}
 
-sudo apt-get install libffi-dev libssl-dev
-sudo apt install python3-dev
+sudo apt-get install -y libffi-dev libssl-dev
+sudo apt install -y python3-dev
 sudo apt-get install -y python3 python3-pip
 
 sudo pip3 install docker-compose
